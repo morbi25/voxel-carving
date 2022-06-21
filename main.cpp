618b4cc141d5c73938776e3a5df9cbebeea03f0f
Ozan Karaali <ozan.karaali@gmail.com>
2022-06-21 13:45:45 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "Eigen.h"
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    // Testing Eigen
+    Eigen::Vector3f v(1, 2, 3);
+    // Print the vector
+    std::cout << "v = " << v << std::endl;
+
+    // Testing OpenCV
+    if (argc != 2)
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    Mat image;
+    image = imread(argv[1], 1);
+    if (!image.data)
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+}
