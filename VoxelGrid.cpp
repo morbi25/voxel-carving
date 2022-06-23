8dae0341a5bfd67658e1acf620e9b7869768363b
Ozan Karaali <ozan.karaali@gmail.com>
2022-06-23 13:07:51 +0200

    Render Voxel Grid with 3 dims of OpenCV images

--- VoxelGrid.cpp
+++ VoxelGrid.cpp
@@ -8,11 +8,11 @@ VoxelGrid::VoxelGrid(int sizeX_, int sizeY_, int sizeZ_)
 	sizeY = sizeY_;
 	sizeZ = sizeZ_;
 
-	grid = new uint8_t * *[sizeZ];
+	grid = new uint8_t **[sizeZ];
 
-	for (int z = 0; z < sizeZ; ++z) 
+	for (int z = 0; z < sizeZ; ++z)
 	{
-		grid[z] = new uint8_t * [sizeX];
+		grid[z] = new uint8_t *[sizeX];
 		for (int x = 0; x < sizeX; ++x)
 		{
 			grid[z][x] = new uint8_t[sizeY];
@@ -21,7 +21,6 @@ VoxelGrid::VoxelGrid(int sizeX_, int sizeY_, int sizeZ_)
 				grid[z][x][y] = 1;
 			}
 		}
-
 	}
 }
 
@@ -49,7 +48,7 @@ void VoxelGrid::carveSingleImg(Mat img)
 	resize(img, resizedImg, Size(sizeX, sizeY));
 	Mat grayImg(sizeX, sizeY, CV_8UC1);
 	cvtColor(resizedImg, grayImg, COLOR_BGR2GRAY);
-	
+
 	for (int z = 0; z < sizeZ; ++z)
 	{
 		for (int x = 0; x < sizeX; ++x)
@@ -59,6 +58,80 @@ void VoxelGrid::carveSingleImg(Mat img)
 				grid[z][x][y] = (grayImg.at<uint8_t>(x, y) == 0) ? 0 : 1;
 			}
 		}
-
 	}
+}
+
+void VoxelGrid::render()
+{
+	Mat zAxis(sizeX, sizeY, CV_8UC1);
+	Mat yAxis(sizeX, sizeZ, CV_8UC1);
+	Mat xAxis(sizeY, sizeZ, CV_8UC1);
+
+	for (int z = 0; z < sizeZ; ++z)
+	{
+		for (int x = 0; x < sizeX; ++x)
+		{
+			for (int y = 0; y < sizeY; ++y)
+			{
+				zAxis.at<uint8_t>(x, y) = 255;
+				yAxis.at<uint8_t>(x, z) = 255;
+				xAxis.at<uint8_t>(y, z) = 255;
+			}
+		}
+	}
+
+	for (int z = 0; z < sizeZ; ++z)
+	{
+		for (int x = 0; x < sizeX; ++x)
+		{
+			for (int y = 0; y < sizeY; ++y)
+			{
+				zAxis.at<uint8_t>(x, y) = (grid[z][x][y] == 1) ? 0 : 1 * zAxis.at<uint8_t>(x, y);
+				yAxis.at<uint8_t>(x, z) = (grid[z][x][y] == 1) ? 0 : 1 * yAxis.at<uint8_t>(x, z);
+				xAxis.at<uint8_t>(y, z) = (grid[z][x][y] == 1) ? 0 : 1 * xAxis.at<uint8_t>(y, z);
+			}
+		}
+	}
+
+	// Invert the pictures.
+
+	for (int x = 0; x < sizeX; ++x)
+	{
+		for (int y = 0; y < sizeY; ++y)
+		{
+			zAxis.at<uint8_t>(x, y) = (zAxis.at<uint8_t>(x, y) == 255) ? 0 : 255;
+		}
+	}
+
+	for (int x = 0; x < sizeX; ++x)
+	{
+		for (int z = 0; z < sizeZ; ++z)
+		{
+			yAxis.at<uint8_t>(x, z) = (yAxis.at<uint8_t>(x, z) == 255) ? 0 : 255;
+		}
+	}
+
+	for (int y = 0; y < sizeY; ++y)
+	{
+		for (int z = 0; z < sizeZ; ++z)
+		{
+			xAxis.at<uint8_t>(y, z) = (xAxis.at<uint8_t>(y, z) == 255) ? 0 : 255;
+		}
+	}
+
+	// Resize the pictures to the 50x to get sharper images.
+	int resize_factor = 50;
+	Mat zAxisResized(resize_factor * sizeX, resize_factor * sizeY, CV_8UC1);
+	Mat yAxisResized(resize_factor * sizeX, resize_factor * sizeZ, CV_8UC1);
+	Mat xAxisResized(resize_factor * sizeY, resize_factor * sizeZ, CV_8UC1);
+
+	resize(zAxis, zAxisResized, Size(resize_factor * sizeX, resize_factor * sizeY), 0, 0, INTER_AREA);
+	resize(yAxis, yAxisResized, Size(resize_factor * sizeX, resize_factor * sizeZ), 0, 0, INTER_AREA);
+	resize(xAxis, xAxisResized, Size(resize_factor * sizeY, resize_factor * sizeZ), 0, 0, INTER_AREA);
+
+	imshow("Z Axis", zAxisResized);
+	imshow("Y Axis", yAxisResized);
+	imshow("X Axis", xAxisResized);
+
+	waitKey(0);
 }
