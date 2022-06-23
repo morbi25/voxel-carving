d0e55915b61d2a76ca9997eb958842d5e1c74511
morbi25 <nils.morbitzer@gmail.com>
2022-06-23 17:03:58 +0200

    Update rectangle size

--- test/Test_ImageLoader.cpp
+++ test/Test_ImageLoader.cpp
@@ -23,12 +23,13 @@ TEST(ImageLoaderTest, ReadSingleImageAndExtractForeground) {
     std::vector<cv::Mat> masks;
     std::vector<Eigen::Matrix4d> transformations;
     std::vector<std::string> filenames = {"../resources/test.png"};
-    std::vector<cv::Rect> rects = {cv::Rect(50, 50, 380, 380)};
+    std::vector<cv::Rect> rects = {cv::Rect(50, 50, 400, 500)};
     std::vector<cv::Mat> foregroundImages;
     
     ImageLoader::readImages(images, transformations, masks, filenames, rects);
     ImageLoader::extractForegroundImages(foregroundImages, images, masks);
 
+    cv::imwrite("../resources/test_foreground.png", foregroundImages[0]);
     cv::imshow("Test Foreground Image", foregroundImages[0]);
     cv::waitKey(0);
     cv::destroyWindow("Test Foreground Image");
