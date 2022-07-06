a324b1075a08bb308de6d200ffa06c234339d3eb
BaranEkin <baranekinozdemir@gmail.com>
2022-07-06 13:11:06 +0200

    Implemented voxel carving algorithm

--- VoxelGrid.cpp
+++ VoxelGrid.cpp
@@ -8,11 +8,20 @@ using namespace cv;
 using namespace open3d;
 using namespace std;
 
-VoxelGrid::VoxelGrid(int sizeX_, int sizeY_, int sizeZ_)
+VoxelGrid::VoxelGrid(int sizeX_, int sizeY_, int sizeZ_, float startX_, float startY_, float startZ_, float step_)
 {
+	// Dimensions of the voxel grid -- Decides number of voxels we have in the voxel grid
 	sizeX = sizeX_;
 	sizeY = sizeY_;
 	sizeZ = sizeZ_;
+
+	// World coordinates of the corner of the voxel grid -- Decides where the voxel grid is placed in the world
+	startX = startX_;
+	startY = startY_;
+	startZ = startZ_;
+
+	// Spacing in between voxels in the world coordnates -- Decides how big the voxel grid is in the world
+	step = step_;
 
 	grid = new uint8_t **[sizeZ];
 
@@ -129,4 +138,71 @@ void VoxelGrid::render()
 	auto vg = std::make_shared<geometry::VoxelGrid>();
 	io::ReadVoxelGrid("voxel_grid.ply", *vg);
 	open3d::visualization::DrawGeometries({vg});
+}
+
+
+cv::Point3f VoxelGrid::voxelToWorld(int x, int y, int z)
+{
+	cv::Point3f point;
+	point.x = startX + step * x;
+	point.y = startY + step * y;
+	point.z = startZ + step * z;
+
+	return point;
+}
+
+cv::Point2i VoxelGrid::projectVoxel(int x, int y, int z, cv::Mat P)
+{
+	cv::Point3f voxelWorld = voxelToWorld(x, y, z);
+	cv::Point2i point;
+	float zc = P.at<float>(2, 0) * voxelWorld.x + P.at<float>(2, 1) * voxelWorld.y + P.at<float>(2, 2) * voxelWorld.z + P.at<float>(2, 3);
+	//float zc = 1.0;
+
+	point.x = (P.at<float>(0, 0) * voxelWorld.x + P.at<float>(0, 1) * voxelWorld.y + P.at<float>(0, 2) * voxelWorld.z + P.at<float>(0, 3)) / zc;
+	point.y = (P.at<float>(1, 0) * voxelWorld.x + P.at<float>(1, 1) * voxelWorld.y + P.at<float>(1, 2) * voxelWorld.z + P.at<float>(1, 3)) / zc;
+
+	return point;
+}
+
+void VoxelGrid::carve(std::vector<cv::Mat> images, std::vector<cv::Mat> PMatrices, float voteTreshold)
+{
+	int numImages = images.size();
+	// Looping over voxels
+	for (int x = 0; x < sizeX; ++x)
+	{
+		for (int y = 0; y < sizeY; ++y)
+		{
+			for (int z = 0; z < sizeZ; ++z)
+			{
+				int vote = 0;
+
+				// Looping over images
+				for (int i = 0; i < numImages; ++i)
+				{
+					// One projection matrix from world to pixel coordinates per image
+					cv::Mat P = PMatrices[i];
+					
+					// Input images need to be grayscale CV_8UC1
+					cv::Mat image = images[i];
+
+					// Project voxel onto image
+					cv::Point2i projectedV = projectVoxel(x, y, z, P);
+
+					if (projectedV.x < image.cols && projectedV.y < image.rows)
+					{
+						// Vote to carve the voxel if projects to background pixel
+						if (image.at<uint8_t>(projectedV.x, projectedV.y) == 0)
+						{
+							++vote;
+						}
+					}
+				}
+				// If enough votes to carve the voxel (1.0 = all votes needed)
+				if (vote >= voteTreshold * numImages)
+				{
+					setElement(x, y, z, 0);
+				}
+			}
+		}
+	}
 }
--- VoxelGrid.h
+++ VoxelGrid.h
@@ -4,7 +4,7 @@
 class VoxelGrid
 {
 public:
-	VoxelGrid(int sizeX_, int sizeY_, int sizeZ_);
+	VoxelGrid::VoxelGrid(int sizeX_, int sizeY_, int sizeZ_, float startX_ = 0.0, float startY_ = 0.0, float startZ_ = 0.0, float step_ = 1.0);
 	~VoxelGrid();
 
 	int getElement(int x, int y, int z);
@@ -14,7 +14,12 @@ public:
 	void toPLY();
 	void render();
 
+	cv::Point3f VoxelGrid::voxelToWorld(int x, int y, int z);
+	cv::Point2i projectVoxel(int x, int y, int z, cv::Mat P);
+	void carve(std::vector<cv::Mat> images, std::vector<cv::Mat> PMatrices, float voteTreshold = 1.0);
+	
 	int sizeX, sizeY, sizeZ;
+	float startX, startY, startZ, step;
 
 private:
 	uint8_t ***grid;
