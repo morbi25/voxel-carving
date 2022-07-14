d497cdac5c0f18bd528769ee907a2f642292acbd
BaranEkin <baranekinozdemir@gmail.com>
2022-07-14 17:31:29 +0200

    Updated main for v2 dataset

--- main.cpp
+++ main.cpp
@@ -8,34 +8,45 @@
 #include "inc/VoxelGrid.h"
 #include "open3d/Open3D.h"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     // NILS's part
-    VoxelGrid voxelGrid(400, 400, 100, 0, 0, 0, 0.0007);
+    VoxelGrid voxelGrid(400, 400, 150, 0, 0, 0, 0.0007);
 
     std::vector<std::filesystem::path> filepaths;
     std::vector<cv::Mat> images;
     std::vector<cv::Mat> results;
     std::vector<cv::Matx44d> cameraPoses;
-    std::string inDir = "../resources/green_teapot_brown_background_subset/";
-    std::string outDir = "../resources/green_teapot_brown_background_preprocessed/";
 
-    // Values of rects are manually determined with GIMP
+    std::string inDir = "../resources/green_teapot_v2/";
+    std::string outDir = "../resources/green_teapot_v2_preprocessed/";
+
     std::unordered_map<std::string, cv::Rect> imageNameToRect{
-        {"20220706_214805.jpg", cv::Rect(3364, 1004, 1848, 1476)},
-        {"20220706_214810.jpg", cv::Rect(3272, 608, 1412, 1560)},
-        {"20220706_214815.jpg", cv::Rect(3070, 677, 17433, 1403)},
-        {"20220706_214821.jpg", cv::Rect(2805, 888, 2121, 1213)},
-        {"20220706_214826.jpg", cv::Rect(3405, 901, 1671, 1592)},
-        {"20220706_214831.jpg", cv::Rect(3468, 683, 1333, 1656)},
-        {"20220706_214838.jpg", cv::Rect(3299, 808, 1505, 1391)},
-        {"20220706_214844.jpg", cv::Rect(2816, 1165, 2245, 1322)},
-        {"20220706_214849.jpg", cv::Rect(2931, 1425, 2501, 2073)},
-        {"20220706_214856.jpg", cv::Rect(3437, 1655, 2481, 2617)},
-        {"20220706_214901.jpg", cv::Rect(3477, 2245, 2273, 2485)},
-        {"20220706_214905.jpg", cv::Rect(2641, 1657, 2241, 2589)},
-        {"20220706_214913.jpg", cv::Rect(3529, 1909, 2117, 2293)},
-        {"20220706_214959.jpg", cv::Rect(2913, 2380, 2881, 3404)},
+        {"20220713_205043.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205047.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205052.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205056.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205101.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205106.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205111.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205115.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205121.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205129.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205134.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205141.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205145.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205151.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205157.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205202.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205210.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205220.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205224.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205230.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205236.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205244.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205249.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205255.jpg", cv::Rect(0, 0, 7990, 5990)},
+        {"20220713_205301.jpg", cv::Rect(0, 0, 7990, 5990)},
     };
     cv::Matx33d cameraMatrix(6005.641173008885, 0, 4030.950098307286, 0, 6002.681113514058, 2986.968236297804, 0, 0, 1);
     cv::Vec<double, 5> distCoeffs(0.08170529228771495, -0.2834249429739051, 0.0007430954776429432, 0.0006295724080059367, 0.3968821057473708);
