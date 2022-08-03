ae7d3d1ca1e8ca8ad75db78c455d8148d80f7b25
morbi25 <nils.morbitzer@gmail.com>
2022-08-03 20:34:06 +0200

    Include parametric threshold using lambda function

--- inc/ForegroundSegmenter.hpp
+++ inc/ForegroundSegmenter.hpp
@@ -37,7 +37,7 @@ public:
      * @param mask 8-bit single-channel mask (the mask is initialized by the function when mode is set to GC_INIT_WITH_RECT)
      * @param scale Scale factor for scaling the rectangle
      */
-    GrabCut(cv::Rect rect=cv::Rect(0, 0, 7990, 5990), unsigned int iterCount = 5, cv::GrabCutModes mode = cv::GC_INIT_WITH_RECT, cv::Mat mask = cv::Mat(), double scale = 0.125);
+    GrabCut(cv::Rect rect = cv::Rect(0, 0, 7990, 5990), unsigned int iterCount = 5, cv::GrabCutModes mode = cv::GC_INIT_WITH_RECT, cv::Mat mask = cv::Mat(), double scale = 0.125);
 
     /**
      * @brief Perform foreground segmentation using grabcut
@@ -51,7 +51,22 @@ public:
 /// Class of a ForegroundSegmenter that uses color thresholding
 class ColorThreshold : public ForegroundSegmenter
 {
+    std::function<bool(double r, double b, double g, double thresholdInterest, double thresholdOther)> mThresholdFct;
+    double mThresholdInterest;
+    double mThresholdOther;
+
 public:
+    /**
+     * @brief Construct a new Color Threshold object
+     *
+     * @param thresholdFct
+     */
+    ColorThreshold(
+        double thresholdInterest = 40, double thresholdOther = 1.3, std::function<bool(double r, double g, double b, double thresholdInterest, double thresholdOther)> thresholdFct = [](double r, double g, double b, double thresholdInterest, double thresholdOther)
+                                                                    {
+    double BRTreshold = g * 1.3;
+    return g < 40 || r + b > BRTreshold; });
+    
     /**
      * @brief Perform foreground segmentation using color thresholding
      *
@@ -61,16 +76,15 @@ public:
     cv::Mat doForegroundSegmentation(const cv::Mat &image) override;
 };
 
-
 /// Class of a ForegroundSegmenter that uses U2Net
 class U2Net : public ForegroundSegmenter
 {
 public:
-     /**
+    /**
      * @brief Perform foreground segmentation using U2Net
      *
      * @param image Image for which the foreground should be extracted
      * @return Foreground image
      */
-    cv::Mat doForegroundSegmentation(const cv::Mat& image) override;
+    cv::Mat doForegroundSegmentation(const cv::Mat &image) override;
 };
